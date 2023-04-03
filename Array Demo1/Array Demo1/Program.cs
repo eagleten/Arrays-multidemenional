3c7eaa6c5c7949345444cde2a6122c6dbdb0279e
eagleten <107332489+eagleten@users.noreply.github.com>
2023-04-02 21:27:44 -0400

    Add files via upload

---
+++ Array Demo1/Array Demo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//WAP to accept 10 numbers in array and print the sum of those numbers
+
+namespace ArrayDemo1
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int sum = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine("Sum of array elements is : {0}", sum);
+
+        }
+    }
+
+}
